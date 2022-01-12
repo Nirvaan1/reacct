00b503d71b7c3d53ced6b97b111937146dbb4762
Nirvaan <nirvaan.guilloux@madagence.com>
2022-01-12 22:50:32 +0100

    react client list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Client from "./Client";
+import ClientForm from "./ClientForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+
+  state = {
+    clients : [
+      {id: 1, nom: "Nirvaan Guilloux"},
+      {id: 2, nom: "Cyril Gane"},
+      {id: 3, nom: "Megane Fox"},
+      {id: 4, nom: "Scarlett Johansson"}
+    ],
+    nouveauClient: "",
+  };
+
+  handleDelete = (id) => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex((client) =>
+        client.id === id
+    );
+
+    clients.splice(index, 1)
+
+    this.setState({clients});
+  }
+
+  handleAdd = client => {
+    const clients = this.state.clients.slice();
+    clients.push(client)
+
+    this.setState({clients})
+  }
+
+  render(){
+    const title = "Liste de clients"
+
+    return(<div>
+      <h1>{title}</h1>
+      <ul>
+        {this.state.clients.map(client => (
+            <Client details={client} onDelete={this.handleDelete}/>
+        ))}
+      </ul>
+      <ClientForm onClientAdd={this.handleAdd}/>
+
+    </div>)
+
+  }
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    rootElement
+);
+
 
 export default App;
---
+++ src/Client.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+const  Client =  ({details, onDelete}) =>
+    (
+        <li>
+            {details.nom}
+            <button onClick={() => onDelete(details.id)}>
+                X</button>
+        </li>
+    );
+
+
+
+
+export default Client;
